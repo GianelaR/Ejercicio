f37bf30cf96fa108a348102fe4ed4c487f1e4e84
Gianela Reyna <54996134+GianelaR@users.noreply.github.com>
2019-11-14 19:46:04 -0500

    Update Ejercicio.java

--- Ejercicio/src/ejercicio/Ejercicio.java
+++ Ejercicio/src/ejercicio/Ejercicio.java
@@ -16,6 +16,10 @@ public class Ejercicio {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        int a=3:
+        
+         
     }
     
 }
